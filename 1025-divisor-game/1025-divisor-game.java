8e72d2f83b6549c767529a8a658255cb930c1ea9
Rival <lunaticxrival@gmail.com>
2026-09-15 15:11:14 +0530

    Time: 12 ms (1.06%), Space: 42.1 MB (51.64%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.java
+++ 1025-divisor-game/1025-divisor-game.java
@@ -1,5 +1,35 @@
 class Solution {
+
+    Boolean[] dp;
+
     public boolean divisorGame(int n) {
-        return n%2==0;
+
+        dp = new Boolean[n + 1];
+
+        return help(n);
+    }
+
+    public boolean help(int n) {
+
+        if (n == 1)
+            return false;
+
+        // Already calculated?
+        if (dp[n] != null)
+            return dp[n];
+
+        for (int i = 1; i < n; i++) {
+
+            if (n % i == 0) {
+
+                if (!help(n - i)) {
+                    dp[n] = true;
+                    return true;
+                }
+            }
+        }
+
+        dp[n] = false;
+        return false;
     }
 }
