92f75eb0ab6821783cc34e27f09b946ac2357827
Rival <lunaticxrival@gmail.com>
2026-09-16 09:56:14 +0530

    Time: 3 ms (86.03%), Space: 43.3 MB (57.35%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,25 +1,25 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (char ch : s.toCharArray()) {
-            if (ch == '(' || ch == '[' || ch == '{') {
-                stack.push(ch);
-            } else {
-                if (stack.isEmpty()) {
+        Stack<Character> st = new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch=='(' || ch=='{' || ch=='['){
+                st.add(ch);
+            }
+            else{
+                if(st.isEmpty()) return false;
+                char top = st.pop();
+
+                if(ch==')' && top=='('|| ch=='}' && top=='{'||ch==']' && top=='['){
+                    continue;
+                  }else{
                     return false;
-                }
-                char top = stack.pop();
-                if (ch == ')' && top != '(') {
-                    return false;
-                }
-                if (ch == ']' && top != '[') {
-                    return false;
-                }
-                if (ch == '}' && top != '{') {
-                    return false;
-                }
+                  }
+                  
+
+
             }
         }
-        return stack.isEmpty();
+
+        return st.isEmpty();
     }
 }
