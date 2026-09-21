8b31249ba3203b565c8b0a3adf4b1ba29742c315
Rival <lunaticxrival@gmail.com>
2026-09-21 10:46:29 +0530

    Time: 0 ms (100%), Space: 43.4 MB (75.93%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,28 +1,29 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int i=0;
-        int j=nums.length-1;
-        int k=0;
-        while(k<=j){
-            if(nums[k]==1){
-                k++;
-            }
-            else if(nums[k]==2){
-                int temp=nums[j];
-                nums[j]=nums[k];
-                nums[k]=temp;
-                j--;
-            }
-            else{
-                int temp=nums[k];
-                nums[k]=nums[i];
-                nums[i]=temp;
-                i++;
-                k++;
+        int i=0,
+            j=nums.length-1,
+            k=0;
+
+            while(k<=j){
+                if(nums[k]==1){
+                    k++;
+                }
+                else if(nums[k]==2){
+                    int temp = nums[k];
+                    nums[k]=nums[j];
+                    nums[j]=temp;
+                    j--;
+                }
+                else{
+                    int t=nums[k];
+                    nums[k]=nums[i];
+                    nums[i]=t;
+                    i++;
+                    k++;
+                }
+                }
+                
 
             }
-        }
-
+            
     }
-    
-}
