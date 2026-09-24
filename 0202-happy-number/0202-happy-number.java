468a9f7effb568863f6e9d69f9776be5bbf018ec
Rival <lunaticxrival@gmail.com>
2026-09-24 15:43:34 +0530

    Time: 1 ms (77.7%), Space: 42.2 MB (78.29%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,9 +1,9 @@
 class Solution {
-    public int Helper(int n){
+    public int sqHelper(int n){
         int sum=0;
         while(n>0){
-            int digit =n%10;
-            sum=sum+(digit * digit);
+            int digit=n%10;
+            sum=sum+(digit*digit);
             n=n/10;
         }
         return sum;
@@ -12,16 +12,14 @@ class Solution {
         int slow=n;
         int fast=n;
         while(fast!=1){
-            slow=Helper(slow);
-            fast=Helper(Helper(fast));
+            slow=sqHelper(slow);
+            fast=sqHelper(sqHelper(fast));
             if(fast==1){
                 return true;
             }
             if(slow==fast){
                 return false;
             }
-
-
         }
         return true;
     }
